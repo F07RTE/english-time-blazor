44cf51cd4f649e7e30f119045fa01a2b2dd1ff91
Guilherme <guilherme.forte17@gmail.com>
2024-07-23 10:26:18 -0300

    Setup Blazor with MudBlazor
    First Acelera

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,13 @@
+using MudBlazor;
+
+namespace EnglishTimeBlazor.Pages
+{
+    public partial class Home
+    {
+        private bool arrows = true;
+        private bool bullets = true;
+        private bool enableSwipeGesture = true;
+        private bool autocycle = true;
+        private Transition transition = Transition.Slide;
+    }
+}
